402ed139b555cf6e66dc3e470b6f7959245417f1
kory33 <6561358+kory33@users.noreply.github.com>
2018-05-09 20:00:15 +0800

    add example listener

--- sponge-ez-data-kotlin/src/test/kotlin/com/github/kory33/util/sponge/exdata/kotlin/example/ExamplePlugin.kt
+++ sponge-ez-data-kotlin/src/test/kotlin/com/github/kory33/util/sponge/exdata/kotlin/example/ExamplePlugin.kt
@@ -4,8 +4,13 @@ import com.github.kory33.util.sponge.exdata.kotlin.example.data.ImmutableStoneBr
 import com.github.kory33.util.sponge.exdata.kotlin.example.data.StoneBreak
 import com.github.kory33.util.sponge.exdata.kotlin.example.data.StoneBreakBuilder
 import com.github.kory33.util.sponge.ezdata.kotlin.buildPartialRegistration
+import com.github.kory33.util.sponge.ezdata.kotlin.extensions.transformSingleValue
 import org.spongepowered.api.Sponge
+import org.spongepowered.api.block.BlockTypes
+import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.Order
+import org.spongepowered.api.event.block.ChangeBlockEvent
 import org.spongepowered.api.event.game.state.GameInitializationEvent
 import org.spongepowered.api.plugin.Plugin
 import org.spongepowered.api.plugin.PluginContainer
@@ -23,6 +28,20 @@ class ExamplePlugin {
         buildPartialRegistration(StoneBreak::class.java, ImmutableStoneBreak::class.java, StoneBreakBuilder())
                 .buildAndRegister(container)
     }
+
+    @Listener(order=Order.POST)
+    fun onStoneBreak(event: ChangeBlockEvent.Break) {
+        val causePlayer = event.cause.mapNotNull { it as? Player }.firstOrNull() ?: return
+
+        val brokeStoneNumber = event.transactions
+                .filter { it.isValid && it.original.state.type == BlockTypes.STONE }
+                .size
+
+        causePlayer.transformSingleValue<Int, StoneBreak> {
+            val result = it + brokeStoneNumber
+            result.also { println(it) }
+        }
+   }
 
     companion object {
         const val ID = "ezdataexampleplugin"
